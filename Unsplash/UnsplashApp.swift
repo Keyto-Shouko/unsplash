00a2e5cfc95210ca8b95858307dcd3db65bc7c72
Bruno <balvesca@gmail.com>
2024-02-02 08:52:38 +0100

    Initial Commit

---
+++ Unsplash/UnsplashApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnsplashApp.swift
+//  Unsplash
+//
+//  Created by ALVES CARVALHO Bruno on 02/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct UnsplashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
